4d979bfa7a761156b0907a231d1124e0042123f4
kematsuda <kenji_matsuda@dwango.co.jp>
2012-10-25 22:30:02 +0900

    smarty_configをclass化

--- app/controller/smarty_config.php
+++ app/controller/smarty_config.php
@@ -1,10 +1,16 @@
 <?php
-ini_set( 'display_errors', 1 );
-define( 'SMARTY_DIR', '/usr/local/lib/Smarty-3.1.12/libs/' );
-require_once( SMARTY_DIR .'Smarty.class.php' );
-$smarty = new Smarty();
+class SmartyConfig
+{
+    public function __construct()
+    {
+        ini_set( 'display_errors', 1 );
+        define( 'SMARTY_DIR', '/usr/local/lib/Smarty-3.1.12/libs/' );
+        require_once( SMARTY_DIR .'Smarty.class.php' );
+        $smarty = new Smarty();
 
-$smarty->template_dir = dirname(__FILE__) . '/../templates/';
-$smarty->compile_dir  = dirname(__FILE__) . '/../../var/smarty/templates_c/';
-$smarty->config_dir   = dirname(__FILE__) . '/../../var/smarty/configs/';
-$smarty->cache_dir   = dirname(__FILE__) . '/../../var/smarty/cache/';
+        $smarty->template_dir = dirname(__FILE__) . '/../templates/';
+        $smarty->compile_dir  = dirname(__FILE__) . '/../../var/smarty/templates_c/';
+        $smarty->config_dir   = dirname(__FILE__) . '/../../var/smarty/configs/';
+        $smarty->cache_dir   = dirname(__FILE__) . '/../../var/smarty/cache/';
+    }
+}
