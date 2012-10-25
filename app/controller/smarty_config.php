c428a023bb0f12c6488853c9a3a81a61d8c6b239
kematsuda <kenji_matsuda@dwango.co.jp>
2012-10-25 22:32:26 +0900

    smarty_configのメソッド修正

--- app/controller/smarty_config.php
+++ app/controller/smarty_config.php
@@ -1,13 +1,15 @@
 <?php
+
+require_once '/usr/local/lib/Smarty-3.1.12/libs/Smarty.class.php';
 class SmartyConfig
 {
     public function __construct()
     {
         ini_set( 'display_errors', 1 );
-        define( 'SMARTY_DIR', '/usr/local/lib/Smarty-3.1.12/libs/' );
-        require_once( SMARTY_DIR .'Smarty.class.php' );
         $smarty = new Smarty();
-
+    }
+    public function setup()
+    {
         $smarty->template_dir = dirname(__FILE__) . '/../templates/';
         $smarty->compile_dir  = dirname(__FILE__) . '/../../var/smarty/templates_c/';
         $smarty->config_dir   = dirname(__FILE__) . '/../../var/smarty/configs/';
