fd3aa6b2d216bc15c356e13029cfced1876e1fcb
kematsuda <kenji_matsuda@dwango.co.jp>
2012-10-25 22:39:35 +0900

    修正

--- app/controller/smarty_config.php
+++ app/controller/smarty_config.php
@@ -1,16 +1,13 @@
 <?php
 
 require_once '/usr/local/lib/Smarty-3.1.12/libs/Smarty.class.php';
-class SmartyConfig
+class SmartyConfig extends Smarty
 {
-    public static function setup()
+    public function setup()
     {
-        ini_set( 'display_errors', 1 );
-        $smarty = new Smarty();
         $smarty->template_dir = dirname(__FILE__) . '/../templates/';
         $smarty->compile_dir  = dirname(__FILE__) . '/../../var/smarty/templates_c/';
         $smarty->config_dir   = dirname(__FILE__) . '/../../var/smarty/configs/';
         $smarty->cache_dir   = dirname(__FILE__) . '/../../var/smarty/cache/';
-        return $smarty;
     }
 }
