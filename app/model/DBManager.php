fbd1d580b41e8077525d09c46210dcc54069c599
kematsuda <kenji_matsuda@dwango.co.jp>
2012-10-25 20:30:55 +0900

    DB操作クラス追加

--- app/model/DBManager.php
+++ app/model/DBManager.php
@@ -2,10 +2,20 @@
 
 class DBManager
 {
+    private $user = null;
+    public $DbName = null;
+
+    public function __construct($DbName)
+    {
+        $file = fopen(dirname(__FILE__) . '/../../conf/db_usser.conf');
+        $this->user = fgets($file);
+        $this->DbName = $DbName;
+    }
+
     public static function q($sql, $binds=array())
     {
         $result = array();
-        $con = new PDO('mysql:dbname=bbs','root','');
+        $con = new PDO('mysql:dbname=' . $this->DbName, $this->user);
         $stmt = $con->prepare($sql);
         $stmt->execute($binds);
         return $stmt->fetchAll();
@@ -13,7 +23,7 @@ class DBManager
 
     public static function save($sql, $binds)
     {
-        $con = new PDO('mysql:dbname=bbs','root','');
+        $con = new PDO('mysql:dbname=' . $this->DbName, $this->user);
         $stmt = $con->prepare($sql);
         $stmt->execute($binds);
     }
